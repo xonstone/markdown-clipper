f5d0d390cd39524009ba6d812b55018ef8a25ce0
xonstone <xonstone@users.noreply.github.com>
2020-05-23 18:46:20 +0200

    Try without tabs permissions

--- background/background.js
+++ background/background.js
@@ -105,26 +105,19 @@ function notify(message) {
 
 function action() {
     if (chrome) {
-        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-            var id = tabs[0].id;
-            chrome.tabs.executeScript(id, {
-                file: "/contentScript/pageScrapper.js"
-            }, function () {
-                console.log("Successfully injected");
-            });
-
+        chrome.tabs.executeScript({
+            file: "/contentScript/pageScrapper.js"
+        }, function () {
+            console.log("Successfully injected");
         });
     } else {
-        browser.tabs.query({currentWindow: true, active: true})
-            .then((tabs) => {
-                var id = tabs[0].id;
-                browser.tabs.executeScript(id, {
-                    file: "/contentScript/pageScrapper.js"
-                }).then(() => {
-                    console.log("Successfully injected");
-                }).catch((error) => {
-                    console.error(error);
-                });
-            });
+        browser.tabs.executeScript({
+            file: "/contentScript/pageScrapper.js"
+        }).then(() => {
+            console.log("Successfully injected");
+        }).catch((error) => {
+            console.error(error);
+        });
+
     }
 }
